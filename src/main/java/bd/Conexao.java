ef3a29d025d49ed6fe4e63f8afbed4c773126165
bieleiji <146545505+bieleiji@users.noreply.github.com>
2026-04-25 21:08:13 -0300

    adição de comentários indicando onde deve ser alterado

--- src/main/java/bd/Conexao.java
+++ src/main/java/bd/Conexao.java
@@ -7,9 +7,12 @@ import java.sql.SQLException;
 public class Conexao {
     public static Connection conectar() {
         try {
-            String url = "jdbc:mysql://localhost:3306/loja";
-            String usuario = "root";
-            String senha = "1234";
+            // preencha esses campos com as informações do seu banco
+
+            String dataBase = "loja"; // ←
+            String url = "jdbc:mysql://localhost:3306/" + dataBase;
+            String usuario = "root";// ←
+            String senha = "1234";// ←
 
             return DriverManager.getConnection(url, usuario, senha);
         } catch (SQLException e) {
